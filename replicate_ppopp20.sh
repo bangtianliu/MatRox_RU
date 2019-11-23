4e6bceb91e5b036e5af2ff7dbf53c96800b7635b
Bangtian Liu <liubangtian@gmail.com>
2019-11-22 20:56:20 -0500

    update scripts

--- replicate_ppopp20.sh
+++ replicate_ppopp20.sh
@@ -29,8 +29,6 @@ cmake ..
 cd sympiler
 make
 
-cp ../../scripts/* .
-
 ### Downloading dataset
 mkdir ../../data
 wget -O ../../data/Points.zip https://www.dropbox.com/sh/ab7f8gut3nh22ym/AAA0QXrC3kS0L4iHS2T0kpg-a?dl=0
@@ -40,6 +38,7 @@ unzip ../../data/Points.zip -d ../../data/
 
 ### Data generation
 if [ $cometORknl -eq 1 ]; then
+cp ../../scripts/* .
 # # Accuracy and Performance tests
 # sbatch testMatRox 0 
 # sbatch testMatRox 0.03
@@ -47,44 +46,52 @@ if [ $cometORknl -eq 1 ]; then
 ### Figure 4: multiple right-hand-sides
 #HSS on comet
 sbatch nrhssh  0.0
+cp hssnrhs.csv ../../Figures/Fig4-nrhs/
 #H2-b on comet
 sbatch nrhssh 0.03
-
+cp h2nrhs.csv ../../Figures/Fig4-nrhs/
 ### Figure 5: optimization breakdown
 #HSS on comet
 sbatch HSSFlops
+cp hssflops.csv ../../Figures/Fig5-flops/
 #H2-b on comet
 sbatch H2Flops
+cp h2flops.csv ../../Figures/Fig5-flops/
 
-### Figure 9:
+
+### Figure 7: Scalability 
+sbatch testScal
+cp scal.csv ../../Figures/Fig7-scal/
+
+### Figure 9:  accuracy
 # #HSS on comet: example command 1
 # sbatch accsh 0  
 # #HSS local: example command 2
 # bash accsh 0
 #H2-b on comet: example command 3
 sbatch accsh 0.03
+cp acc.csv ../../Figures/Fig9-acc/
 # #H2-b local: example command 4
 # bash accsh 0.03
 
-### Figure 7: Scalability 
-#HSS on comet
-#sbatch testScal
 
 ### Figure 10: multiple runs
 # #HSS on comet
 # sbatch nrunsh 0.0
 #H2-b
 sbatch nrunsh 0.03
+cp nrun.csv ../../Figures/Fig10-nrun/
 
 fi
 
-if [ $cometORknl -eq 2 ]; then
-sbatch testScalKNL
-fi
+# if [ $cometORknl -eq 2 ]; then
+# sbatch testScalKNL
+# fi
 
 ### Library tests
 cd ../../libTest/
 if [ $cometORknl -eq 1 ]; then
+cp ../scripts/* .
 # # Accuracy and Performance tests
 # #GOFMM for HSS
 # sbatch testGOFMM 0
@@ -93,62 +100,83 @@ if [ $cometORknl -eq 1 ]; then
 
 # Figure 4
 #HSS (GOFMM)
-sbatch GOnrhssh 0.0
+sbatch GOnrhsh 0.0
+cp hssnrhs.csv ../Figures/Fig4-nrhs/
 #H2b (GOFMM)
 sbatch GOnrhssh 0.03
+cp h2nrhs.csv ../Figures/Fig4-nrhs/
 #HSS (STRUMPACK)
 sbatch stnrhsh
+cp stnrhs.csv ../Figures/Fig4-nrhs/
 
 #Figure 5
 #HSS  (GOFMM)
 sbatch testGOFlops 0.0
+cp gohssflops.csv ../Figures/Fig5-flops/
 #H2-b (GOFMM)
 sbatch testGOFlops 0.03
+cp goh2flops.csv ../Figures/Fig5-flops/
 
 #HSS (STRUMPACK)
 sbatch testSTFlops
+cp stflops.csv ../Figures/Fig5-flops/
 
 #Figure 7
 sbatch testGOScal
+cp goscal.csv ../Figures/Fig7-scal/
 sbatch testSTScal
+cp stscal.csv ../Figures/Fig7-scal/
 sbatch testSMAScal
-
+cp smascal.csv ../Figures/Fig7-scal/
 
 # Figure 10
 sbatch GOnrunsh 0.03
+cp gonrun.csv ../Figures/Fig10-nrun/
 fi
 
 if [ $cometORknl -eq 2 ]; then
+cp ../../scripts/KNL/testScalKNL ./
 # Figure 7
+sbatch testScalKNL
+cp scalknl.csv ../../Figures/Fig7-scal/
+cd ../../libTest/
 sbatch testGOScal
+cp goscalknl.csv ../../Figures/Fig7-scal/
 sbatch testSTScal 
+cp stscalknl.csv ../../Figures/Fig7-scal/
 fi
 
 ### Plotting graphs
 if [ $cometORknl -eq 1 ]; then
+cd ../Figures/Fig4-nrhs/
 #Figure 4-HSS
 python drawhssnrhs.py --s hssnrhs.csv --g gohssnrhs.csv --t stnrhs.csv
 
 #Figure 4-H2b
 python drawh2bnrhs.py --s h2nrhs.csv --g goh2nrhs.csv
 
+cd ../Fig5-flops/
 #Figure 5-HSS
 python drawhssflops.py --s hssflops.csv --g gohssflops.csv --t stflops.csv
 
 #Figure 5-H2b
 python drawh2bflops.py --s h2flops.csv --g goh2flops.csv
 
+cd ../Fig7-scal/
 #Figure 7
 python drawscal.py --m scal.csv --g goscal.csv --s stscal.csv --sa smascal.csv
 
+cd ../Fig9-acc/
 #Figure 9
 python drawacc.py acc.csv
 
+cd ../Fig10-nrun/
 #Figure 10
 python drawnrun.py --m nrun.csv --g gonrun.csv
 
 fi
 
 if [ $cometORknl -eq 2 ]; then
+cd ../Figures/Fig7-scal/
 python drawscalknl.py --m scalknl.csv --g goscalknl.csv --s stscalknl.csv
 fi
